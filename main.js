68f7ceaa2d489c04ca98fc1f09c5cd1218cbf2be
moneambenbelgacem <moneem.benbelgasem@esen.tn>
2022-10-28 21:30:35 +0200

    first_code_js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let title = "coding", description ="this coding camp free", date= "25/10"
 let card =` 
 <div class ="card">
 <h3> Hello ${title} </h3>
-<p>${description}</p>
+<p>${description} </p>
 <span>${date}</span>
 </div>`
 document.write(card.repeat(4));
